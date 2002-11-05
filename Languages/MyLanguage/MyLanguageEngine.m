7f265245c16d66ed0fe47edd9b501b0899328669
Stefan Urbanek <stefan@agentfarms.net>
2002-11-05 20:04:24 +0000

    Added MyLanguage example
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/steptalk/trunk@14928 72102866-910b-0410-8b05-ffd578937521

---
+++ Languages/MyLanguage/MyLanguageEngine.m
@@ -0,0 +1,27 @@
+/**
+    MyLanguageEngine
+ 
+    @COPYRIGHT@
+ 
+    Written by: @AUTHOR@
+    Date: @DATE@
+ 
+ */
+
+#import "MyLanguageEngine.h"
+
+@implementation MyLanguageEngine
+- (id) executeCode:(NSString *)sourceCode 
+       inEnvironment:(STEnvironment *)env
+{
+    id                     retval = nil;
+
+    /* execute the code sourceCode using environment env */
+    /* use [env objectDictionary] to get environment objects 
+       (see STEnvironment.h to see what you can do) */
+    
+    /*    retval = return value from the interpreter; */
+    
+    return retval;
+}
+@end
