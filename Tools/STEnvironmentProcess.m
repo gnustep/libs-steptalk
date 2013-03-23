d51cc437552dcdc50dfa65a4bb7d2fd8d507a050
Wolfgang Lux <wolfgang.lux@gmail.com>
2013-03-23 19:17:07 +0000

    Check the result of the super class initializer and assign it to self.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/steptalk/trunk@36407 72102866-910b-0410-8b05-ffd578937521

--- Tools/STEnvironmentProcess.m
+++ Tools/STEnvironmentProcess.m
@@ -7,25 +7,25 @@
 @implementation STEnvironmentProcess
 - initWithDescriptionName:(NSString *)descName
 {
-    STEnvironmentDescription *desc;
-    
-    self = [super init];
-
-    if(descName)
+    if ((self = [super init]) != nil)
     {
-        NSLog(@"Creating environment from description '%@'", descName);
-        desc = [STEnvironmentDescription descriptionWithName:descName];
-        environment = [[STEnvironment alloc] initWithDescription:desc];
-    }
-    else
-    {
-        environment = [[STEnvironment alloc] initWithDefaultDescription];
-    }
-    
-    /* FIXME: use some configurable mechanism */
-    [environment setObject:environment forName:@"Environment"];
-    [environment loadModule:@"SimpleTranscript"];
+        STEnvironmentDescription *desc;
 
+        if (descName)
+        {
+            NSLog(@"Creating environment from description '%@'", descName);
+            desc = [STEnvironmentDescription descriptionWithName:descName];
+            environment = [[STEnvironment alloc] initWithDescription:desc];
+        }
+        else
+        {
+            environment = [[STEnvironment alloc] initWithDefaultDescription];
+        }
+
+        /* FIXME: use some configurable mechanism */
+        [environment setObject:environment forName:@"Environment"];
+        [environment loadModule:@"SimpleTranscript"];
+    }
     return self;
 }
 - (void)dealloc
