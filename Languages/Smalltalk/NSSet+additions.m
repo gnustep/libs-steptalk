1682f849a4bb1e3a5ce5dcf686b0fc94e769c8d3
Wolfgang Lux <wolfgang.lux@gmail.com>
2025-04-06 17:53:51 +0200

    Implement Smalltalk collection protocol methods for NSSet

---
+++ Languages/Smalltalk/NSSet+additions.m
@@ -0,0 +1,134 @@
+/**
+    NSSet-additions.m
+    Various methods for NSSet
+
+    Copyright (c) 2002 Free Software Foundation
+
+    Written by: Stefan Urbanek <urbanek@host.sk>
+
+    This file is part of the StepTalk project.
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ */
+
+#import "NSSet+additions.h"
+#import "NSNumber+additions.h"
+#import "STBlock.h"
+
+#import <Foundation/NSAutoreleasePool.h>
+#import <Foundation/NSEnumerator.h>
+
+#import <StepTalk/STExterns.h>
+
+@implementation NSSet (STCollecting)
+- do:(STBlock *)block
+{
+    NSEnumerator *enumerator;
+    id            object;
+    id            retval = nil;
+
+    enumerator = [self objectEnumerator];
+    while ((object = [enumerator nextObject]))
+    {
+        retval = [block value:object];
+    }
+
+    return retval;
+}
+
+- select:(STBlock *)block
+{
+    NSMutableSet *set;
+    NSEnumerator *enumerator;
+    id            object;
+    id            value;
+
+    set = [NSMutableSet set];
+
+    enumerator = [self objectEnumerator];
+    while ((object = [enumerator nextObject]))
+    {
+        value = [block value:object];
+        if ([(NSNumber *)value isTrue])
+        {
+            [set addObject:object];
+        }
+    }
+
+    return [NSSet setWithSet:set];
+}
+
+- reject:(STBlock *)block
+{
+    NSMutableSet *set;
+    NSEnumerator *enumerator;
+    id            object;
+    id            value;
+
+    set = [NSMutableSet set];
+
+    enumerator = [self objectEnumerator];
+    while ((object = [enumerator nextObject]))
+    {
+        value = [block value:object];
+        if ([(NSNumber *)value isFalse])
+        {
+            [set addObject:object];
+        }
+    }
+
+    return [NSSet setWithSet:set];
+}
+
+- collect:(STBlock *)block
+{
+    NSMutableSet *set;
+    NSEnumerator *enumerator;
+    id            object;
+    id            value;
+
+    set = [NSMutableSet set];
+
+    enumerator = [self objectEnumerator];
+    while ((object = [enumerator nextObject]))
+    {
+        value = [block value:object];
+	if (value == nil)
+	    value = STNil;
+        [set addObject:value];
+    }
+
+    return [NSSet setWithSet:set];
+
+}
+- detect:(STBlock *)block
+{
+    NSEnumerator *enumerator;
+    id            object;
+    id            retval;
+
+    enumerator = [self objectEnumerator];
+    while ((object = [enumerator nextObject]))
+    {
+        retval = [block value:object];
+        if ([(NSNumber *)retval isTrue])
+        {
+            return object;
+        }
+    }
+    return nil;
+}
+@end
